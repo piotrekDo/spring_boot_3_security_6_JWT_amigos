a155ae48e14355e8c58d0bd1132a349150817d3f
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-02-15 09:19:59 +0100

    initial commit

---
+++ notatki.md
@@ -0,0 +1,1 @@
+# Spring Boot 3, Security 6, JWT
---
+++ src/main/java/com/example/spring_boot_3_security_6_jwt_amigos/SpringBoot3Security6JwtAmigosApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_3_security_6_jwt_amigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3Security6JwtAmigosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3Security6JwtAmigosApplication.class, args);
+    }
+
+}
