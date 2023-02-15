3e87fa2755b50c836a5c0eb25a8c3fdfe2e52a90
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-02-15 16:50:07 +0100

    h2 console fix

--- src/main/java/com/example/spring_boot_3_security_6_jwt_amigos/config/SecurityConfig.java
+++ src/main/java/com/example/spring_boot_3_security_6_jwt_amigos/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.spring_boot_3_security_6_jwt_amigos.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -24,9 +27,11 @@ public class SecurityConfig {
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeHttpRequests().requestMatchers("/h2-console/**").permitAll();
+        http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+        );
         http.authorizeHttpRequests().requestMatchers("/auth/**").permitAll();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.authorizeHttpRequests().anyRequest().authenticated();
         http.authenticationProvider(authenticationProvider);
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
